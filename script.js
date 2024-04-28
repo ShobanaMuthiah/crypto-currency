238db5379222dd410de3a2aadd40bfcf2a6e6946
ShobanaMuthiah <shobanam222@gmail.com>
2024-04-27 12:35:54 -1200

    updated

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const cur=document.getElementById("currency");
+const name2=document.getElementsByName("currency");
 const details=document.getElementById("details");
 const getdetails=document.getElementById("getdetails");
+const err=document.getElementById("err")
 
 getdetails.addEventListener("click",async ()=>{
-    const inputid=currency.value;
+    const inputid=cur.value;
+    try{
     const data=await fetch(`https://api.coincap.io/v2/assets/${inputid}`);
     const res=await data.json();
-    
+       
+        if(inputid===null||inputid===''){
+            throw new Error ("Please enter an Input value")
+        }
         getDetails(res);
+}
+catch(error){
+    if(error instanceof TypeError){
+        err.innerHTML=`Error: Enter all letters in lowercase`
+    }
+    else{
+        err.innerHTML=`Error: ${error.message}`}
+}
 })
 function getDetails(res){
     const d=res.data
+    // console.log(d);
 details.innerHTML=`
 <div class="card ">
 <div class="title row row-cols-1 row-cols-sm-2 g-lg-2 g-md-4 mb-lg-2 " ><div class="col lab">Currency:</div><div class="col cval "> ${d.name}</div>
